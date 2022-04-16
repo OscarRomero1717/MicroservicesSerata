0795d99049a8ac02608fac4d1c27c4d09ab2d108
Oscar Romero <odromero1717@gmail.com>
2022-04-16 16:44:06 -0500

    Fist commit

---
+++ SERATACommerce/src/Services/Catalog/Catalog_API/Controllers/Default.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog_API.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class Default : ControllerBase
+    {
+        
+        [HttpGet]
+        public string Get()
+        {
+            return "start up";
+        }
+    }
+}
---
+++ SERATACommerce/src/Services/Catalog/CatologDomain/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatologDomain
+{
+    public class Product
+    {
+         public int IdProduct { get; set; }
+
+        public string NameProduct { get; set; } 
+
+    }
+}
